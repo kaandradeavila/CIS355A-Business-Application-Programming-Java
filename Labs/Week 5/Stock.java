24fad43e176214a8536c516685f8399c6abd02e8
kaandradeavila <141256253+kaandradeavila@users.noreply.github.com>
2023-08-03 12:26:36 -0400

    Update Stock.java

--- Labs/Week 5/Stock.java
+++ Labs/Week 5/Stock.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /****************************************************************************
  Program Name: Stocks.java
  Programmer's Name: Kyrenia Ailen Andrade Avila
