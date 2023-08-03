ad0dfd5fe4eb6ea816c6a3ad0746dbd97a35a9f5
kaandradeavila <141256253+kaandradeavila@users.noreply.github.com>
2023-08-03 12:34:37 -0400

    Update Customer.java

--- Landscaping Project/Week 3 - Advanced GUI/Customer.java
+++ Landscaping Project/Week 3 - Advanced GUI/Customer.java
@@ -1,10 +1,4 @@
-
 import java.text.DecimalFormat;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /****************************************************************************
  Program Name: Customer.java
